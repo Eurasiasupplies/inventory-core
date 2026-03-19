a3ad50bb8b4969db6710cd9bef3810abe8099ab8
Ashad Bappy <bappyeurasia@gmail.com>
2026-03-19 14:39:05 +0600

    feat('backend'): History reference added

--- src/Contracts/StockHistoryInterface.php
+++ src/Contracts/StockHistoryInterface.php
@@ -4,7 +4,7 @@ namespace InventoryCore\Contracts;
 
 interface StockHistoryInterface
 {
-    public function storeTransferHistory(int $productId, int $warehouseId, int $quantity, int $oldquantity, int $onlineQuantity);
-    public function storeOnlineHistory(int $productId, int $quantity, int $oldQuantity);
+    public function storeTransferHistory(int $referenceId, int $productId, int $warehouseId, int $quantity, int $oldQuantity, int $onlineQuantity);
+    public function storeOnlineHistory(int $referenceId, int $productId, int $quantity, int $oldQuantity);
     public function historyLog(array $data, int $quantity, string $action, string $type);
 }
